297bd7cbe54a6fecf4ebded36300e472d6475672
Edvo1901 <DongVo1901@gmail.com>
2024-04-03 19:59:23 +1030

    Complete implementing lesson selection page

---
+++ components/lessonSelection/lessonSelection.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('lessonSelectionNavigate').addEventListener('click', function() {
+        window.location.href = '../thPage/thPage.html';
+    });
+});
