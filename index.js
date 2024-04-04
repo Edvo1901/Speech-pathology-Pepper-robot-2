177bdd18bea87bb3584c1f9a191a128fde0db156
dongvo191 <DongVo1901@gmail.com>
2024-04-04 13:21:51 +1030

    pepper connect testing

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
+import QiSession from 'QiSession'
+
+QiSession(function (s) {
+    console.log("connected!");
+    // you can now use your QiSession
+    session = s
+}, function () {
+    console.log("disconnected");
+});
+
+document.addEventListener('DOMContentLoaded', function () {
     'use strict';
     var forms = document.querySelectorAll('.needs-validation');
 
-    Array.prototype.forEach.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
+    Array.prototype.forEach.call(forms, function (form) {
+        form.addEventListener('submit', function (event) {
             if (!form.checkValidity()) {
                 event.preventDefault();
                 event.stopPropagation();
@@ -13,3 +23,8 @@ document.addEventListener('DOMContentLoaded', function() {
         }, false);
     });
 });
+
+RobotUtils.onServices(function (ALLeds, ALTextToSpeech) {
+    ALTextToSpeech.say("Tell us more about your experience.");
+});
+
