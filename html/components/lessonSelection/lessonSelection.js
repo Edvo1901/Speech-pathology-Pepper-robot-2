18fb736447b8fa84f06d5954756e982382b29bcb
Edvo1901 <DongVo1901@gmail.com>
2024-04-12 09:02:05 +0930

    adding header to lesson page

--- html/components/lessonSelection/lessonSelection.js
+++ html/components/lessonSelection/lessonSelection.js
@@ -1,5 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('lessonSelectionNavigate').addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('lessonSelectionNavigate').addEventListener('click', function () {
         window.location.href = '../thPage/thPage.html';
     });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetch("../header/header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header-placeholder").innerHTML = data;
+        });
 });
