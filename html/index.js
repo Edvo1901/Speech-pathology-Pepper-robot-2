4f47a3fa9b59756a0f3f73094b512f3bcf2a1c86
Edvo1901 <DongVo1901@gmail.com>
2024-04-12 08:43:10 +0930

    fix consent page as require

--- html/index.js
+++ html/index.js
@@ -1,15 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    'use strict';
-    var forms = document.querySelectorAll('.needs-validation');
 
-    Array.prototype.forEach.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-            if (!form.checkValidity()) {
-                event.preventDefault();
-                event.stopPropagation();
-            }
-
-            form.classList.add('was-validated');
-        }, false);
-    });
-});
