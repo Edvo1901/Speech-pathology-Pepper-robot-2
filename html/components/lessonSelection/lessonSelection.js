a7ec08a94adb2375a1fe509c79193aff61d34a0f
dongvo191 <DongVo1901@gmail.com>
2024-04-22 15:15:52 +0930

    header int

--- html/components/lessonSelection/lessonSelection.js
+++ html/components/lessonSelection/lessonSelection.js
@@ -2,4 +2,12 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('lessonSelectionNavigate').addEventListener('click', function () {
         window.location.href = '../thPage/thPage.html';
     });
+
+    fetch('../header/header.html')
+  .then(response => response.text())
+  .then(html => {
+      document.getElementById('header-placeholder').innerHTML = html;
+  })
+  .catch(error => console.error('Error loading the header:', error));
 });
+
