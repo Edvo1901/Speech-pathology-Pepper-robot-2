d0cba48bb7d028e3d3c2e8142182874ed71887d0
LhhGordon <83793601+LhhGordon@users.noreply.github.com>
2024-04-22 14:45:40 +0930

    start building step eight

--- html/components/syllableLevelPage/syllableLevelPage.js
+++ html/components/syllableLevelPage/syllableLevelPage.js
@@ -1,3 +1,24 @@
+var session = new QiSession(function () {
+    session = s;
+}, disconnected,location.host);
+
+function connected(s) {
+    //If you want to subscribe so some events (to send info pepper->tablet) call the function here
+}
+function disconnected(error) {
+    console.log("Session disconnected");
+    displayDiv.textContent = "Session disconnected";
+}
+
+session.service("ALMemory").done(function (memory) {
+
+    //this dont work, research more
+    memory.raiseEvent("stepFive", "step five is running");
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('backToPreviousPage').addEventListener('click', function() {
         window.location.href = '../breakPage/stretchBreak.html';
@@ -10,4 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('nextStep').addEventListener('click', function() {
         window.location.href = '../breakPage/movementBreak.html';
     });
+});
+
+
 });
