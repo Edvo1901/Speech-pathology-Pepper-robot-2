f8b6a90388f42f0f711325e6859213b147b22d13
Edvo1901 <DongVo1901@gmail.com>
2024-04-22 13:19:43 +0930

    new word level page

---
+++ html/components/wordLevelPage/wordLevelPage.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('backToPreviousPage').addEventListener('click', function() {
+        window.location.href = '../breakPage/movementBreak.html';
+    });
+
+    document.getElementById('backToHomePage').addEventListener('click', function() {
+        window.location.href = '../lessonSelection/lessonSelection.html';
+    });
+
+    document.getElementById('nextStep').addEventListener('click', function() {
+        window.location.href = './wordLevelPage.html';
+    });
+});
