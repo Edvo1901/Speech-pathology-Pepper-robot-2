776b2f0e530c4eed0356191060b5010fb7cd753a
LhhGordon <83793601+LhhGordon@users.noreply.github.com>
2024-04-22 15:01:15 +0930

    testing button function on step 8

--- html/components/syllableLevelPage/syllableLevelPage.js
+++ html/components/syllableLevelPage/syllableLevelPage.js
@@ -2,24 +2,26 @@ var session = new QiSession(function () {
     session = s;
 }, disconnected,location.host);
 
-function connected(s) {
-    //If you want to subscribe so some events (to send info pepper->tablet) call the function here
-}
-function disconnected(error) {
-    console.log("Session disconnected");
-    displayDiv.textContent = "Session disconnected";
-}
+function connected(s) {}
+function disconnected(error) {console.log("Session disconnected");}
 
-session.service("ALMemory").done(function (memory) {
+// session.service("ALMemory").done(function (memory) {
 
-    //this dont work, research more
-    memory.raiseEvent("stepFive", "step five is running");
+//     //this dont work, research more
+//     memory.raiseEvent("stepEight", "step eight is running")
+
+function ee(session)
+{
+    session.service("ALTextToSpeech").done(function (tts) {
+        tts.say("ee as in beach");
+    }).fail(function (error) {
+    console.log("An error occurred:", error);
+    });
+};
 
 
 
-
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function(session) {
     document.getElementById('backToPreviousPage').addEventListener('click', function() {
         window.location.href = '../breakPage/stretchBreak.html';
     });
@@ -31,7 +33,4 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('nextStep').addEventListener('click', function() {
         window.location.href = '../breakPage/movementBreak.html';
     });
-});
-
-
 });
