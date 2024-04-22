ca9fcf9eb331e9278ac82d63a18326068c0e39ca
Edvo1901 <DongVo1901@gmail.com>
2024-04-22 13:13:37 +0930

    fix page navigation

--- html/components/thPage/thPage.js
+++ html/components/thPage/thPage.js
@@ -2,4 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('isolationPageNavigate').addEventListener('click', function() {
         window.location.href = '../isolationLevelPage/isolationLevelPage.html';
     });
+
+    document.getElementById('syllablePageNavigate').addEventListener('click', function() {
+        window.location.href = '../syllableLevelPage/syllableLevelPage.html';
+    });
 });
