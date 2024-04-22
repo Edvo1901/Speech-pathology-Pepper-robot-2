3cfc6950ce4bc7904233de31422ed165f9c54ade
dongvo191 <DongVo1901@gmail.com>
2024-04-22 15:19:36 +0930

    testing header

--- html/components/lessonSelection/lessonSelection.js
+++ html/components/lessonSelection/lessonSelection.js
@@ -3,11 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
         window.location.href = '../thPage/thPage.html';
     });
 
-    fetch('../header/header.html')
-  .then(response => response.text())
-  .then(html => {
-      document.getElementById('header-placeholder').innerHTML = html;
-  })
-  .catch(error => console.error('Error loading the header:', error));
+    $('#header-placeholder').load('../header/header.html');
 });
 
