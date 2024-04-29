28cf9af0816b10f0610ffda91791dfb3072a8955
dongvo191 <DongVo1901@gmail.com>
2024-04-29 15:56:20 +0930

    testing load header

--- html/components/wordLevelPage/wordLevelPage.js
+++ html/components/wordLevelPage/wordLevelPage.js
@@ -23,27 +23,5 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = './final/finalPosition.html';
     });
 
-    if (!window.fetch) {
-        var script = document.createElement('script');
-        script.src = 'https://cdn.jsdelivr.net/npm/whatwg-fetch@3.6.2/fetch.min.js';
-        script.onload = function() {
-            console.log('Fetch polyfill loaded successfully.');
-            loadHeader();
-        };
-        script.onerror = function() {
-            console.error('Error loading fetch polyfill.');
-        };
-        document.head.appendChild(script);
-    } else {
-        loadHeader();
-    }
+    $("#header-placeholder").load("../header/header.html");
 });
-
-function loadHeader() {
-    fetch('path/to/header.html')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('header-placeholder').innerHTML = html;
-        })
-        .catch(error => console.error('Failed to load header: ', error));
-}
