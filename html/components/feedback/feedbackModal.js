a356a22ad16982acd429f381fed61232cb910de5
dongvo191 <DongVo1901@gmail.com>
2024-05-27 16:18:18 +0930

    test changes

--- html/components/feedback/feedbackModal.js
+++ html/components/feedback/feedbackModal.js
@@ -40,3 +40,42 @@ $(document).ready(function () {
         }
 	});
 });
+
+function getCurrentDate() {
+    var date = new Date();
+    var day = String(date.getDate()).padStart(2, "0");
+    var month = String(date.getMonth() + 1).padStart(2, "0");
+    var year = date.getFullYear();
+    return `${day}-${month}-${year}`;
+}
+
+// Function to send an email with feedback data
+function sendEmail(enteredEmail) {
+	// Retrieve feedback data from local storage
+	var feedbackData = localStorage.getItem("feedbackData");
+	feedbackData = feedbackData ? JSON.parse(feedbackData) : {};
+
+	// Convert feedback data to a readable format
+	var feedbackText = "";
+	for (var [key, value] of Object.entries(feedbackData)) {
+		feedbackText += `${key}: ${value}\n`;
+	}
+
+    // Email template
+	var templateParams = {
+		sendEmail: enteredEmail,
+		date: getCurrentDate(),
+		message: `Feedback data:\n\n${feedbackText}`,
+	};
+
+	emailjs.send("pepperFeedbackEmail", "weeklyEmailTemp", templateParams).then(
+		function (response) {
+			//console.log("SUCCESS!", response.status, response.text);
+            alert("Email sent!")
+		},
+		function (error) {
+			//console.log("FAILED...", error);
+            alert("Something went wrong!!")
+		}
+	);
+}
