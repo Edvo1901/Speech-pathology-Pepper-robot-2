768b31ece045aaaf237860005b76a0e3dc361673
dongvo191 <DongVo1901@gmail.com>
2024-05-27 16:02:09 +0930

    seperate the logic

--- html/components/feedback/feedback.js
+++ html/components/feedback/feedback.js
@@ -1,10 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-	document
-		.getElementById("homeButton")
-		.addEventListener("click", function () {
-			window.location.href = "../lessonSelection/lessonSelection.html";
-		});
-
 	document.querySelectorAll(".feedback-image").forEach((image) => {
 		image.addEventListener("click", function () {
 			var feedbackType = this.alt;
